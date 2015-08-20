3fae224b405db64ed98db3eec12ba3536368394e
jbloom <josh@wise.io>
2015-08-20 14:11:21 -0700

    oopII files

--- DataFiles_and_Notebooks/09_OOPII/catcherr.py
+++ DataFiles_and_Notebooks/09_OOPII/catcherr.py
@@ -3,10 +3,10 @@ try:
     f = open('myfile.txt')
     s = f.readline()
     i = int(s.strip())
-except IOError as (errno, strerror):
-    print "I/O error(%i): %s" % (errno, strerror)
+except IOError as err:
+    print("I/O error: %s" % err)
 except ValueError:
-    print "Could not convert data to an integer."
+    print("Could not convert data to an integer.")
 except:
-    print "Unexpected error:", sys.exc_info()[0]
+    print("Unexpected error:", sys.exc_info()[0])
     raise
