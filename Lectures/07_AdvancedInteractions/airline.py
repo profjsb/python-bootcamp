a5cccefe9a925a0890aa79cd240c5a81232aaff6
jbloom <josh@wise.io>
2016-08-21 16:10:57 -0700

    copied notebook for 07 to lecture folder

---
+++ Lectures/07_AdvancedInteractions/airline.py
@@ -0,0 +1,10 @@
+airports = {"DCA": "Washington, D.C.", "IAD": "Dulles", "LHR": "London-Heathrow", \
+            "SVO": "Moscow", "CDA": "Chicago-Midway", "SBA": "Santa Barbara", "LAX": "Los Angeles",\
+            "JFK": "New York City", "MIA": "Miami", "AUM": "Austin, Minnesota"}
+            
+# airline, number, heading to, gate, time (decimal hours) 
+flights = [("Southwest",145,"DCA",1,6.00),("United",31,"IAD",1,7.1),("United",302,"LHR",5,6.5),\
+           ("Aeroflot",34,"SVO",5,9.00),("Southwest",146,"CDA",1,9.60), ("United",46,"LAX",5,6.5),\
+           ("Southwest",23,"SBA",6,12.5),("United",2,"LAX",10,12.5),("Southwest",59,"LAX",11,14.5),\
+           ("American", 1,"JFK",12,11.3),("USAirways", 8,"MIA",20,13.1),("United",2032,"MIA",21,15.1),\
+           ("SpamAir",1,"AUM",42,14.4)]
